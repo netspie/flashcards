be244e7e5b6010ade6b629b892fefafaeba66468
netspie <dariusz.lusnia@gmail.com>
2024-05-31 17:23:14 +0200

    Add project

---
+++ FlashcardApp/FlashcardApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace FlashcardApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ FlashcardApp/FlashcardApp/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace FlashcardApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
