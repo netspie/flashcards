50d4d6548f7db1feba7c5aa5865d8c13b8fb5c62
netspie <dariusz.lusnia@gmail.com>
2024-06-08 18:06:12 +0200

    Improve items data grid.. ranged from files

--- FlashcardApp/FlashcardApp/Services/ItemService.cs
+++ FlashcardApp/FlashcardApp/Services/ItemService.cs
@@ -10,4 +10,22 @@ internal record ItemService(IRepository<Item> Repository)
     public Task<bool> Create(Item entity) => Repository.Create(entity);
     public Task<bool> Update(Item entity) => Repository.Update(entity);
     public Task<bool> Delete(string id) => Repository.Delete(id);
+
+    public async Task<(Item[] items, RangeDTO range)> GetRange(RangeArg range)
+    {
+        //var dataPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "/FlashcardApp/items";
+        var items = await Repository.GetAll();
+        var itemsRanged = items.Skip(range.Start).Take(range.Limit).ToArray();
+
+        return (itemsRanged, new(items.Length, range.Start, range.Limit));
+    }
 }
+
+public record RangeDTO(
+    int TotalCount,
+    int Start,
+    int Limit);
+
+public record RangeArg(
+    int Start,
+    int Limit);
