e4e243aaeef597df5a31fe092e5cafb60f606ddf
netspie <dariusz.lusnia@gmail.com>
2024-06-08 22:20:23 +0200

    Order item fields by item template order

---
+++ FlashcardApp/FlashcardApp/Common/CollectionExtensions.cs
@@ -0,0 +1,23 @@
+﻿namespace FlashcardApp.Common;
+
+public static class EnumerableExtensions
+{
+    public static int IndexWhere<T>(this IEnumerable<T> source, Func<T, bool> condition)
+    {
+        if (source is null)
+            return -1;
+
+        var len = source.Count();
+        int i = 0;
+
+        foreach (var item in source)
+        {
+            if (condition(item))
+                return i;
+
+            i++;
+        }
+
+        return -1;
+    }
+}
