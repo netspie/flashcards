26b6f95bf7bdce791f06db111bad089d3ca13f34
Dariusz Luśnia <dariusz.lusnia@gmail.com>
2025-05-02 20:22:28 +0900

    Refactor property builder conversion

--- src/flashcards-blazor/Flashcards.WebApp/Features/Projects/ProjectConfiguration.cs
+++ src/flashcards-blazor/Flashcards.WebApp/Features/Projects/ProjectConfiguration.cs
@@ -1,8 +1,7 @@
 ﻿using Flashcards.WebApp.Features.Account;
+using Flashcards.WebApp.Shared.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
-using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
-using System.Linq.Expressions;
 
 namespace Flashcards.WebApp.Features.Projects;
 
@@ -11,24 +10,7 @@ public class ProjectConfiguration : IEntityTypeConfiguration<Project>
     public void Configure(EntityTypeBuilder<Project> builder)
     {
         builder.HasKey(x => x.Id);
-        builder.Property(x => x.Id)
-            .HasConversion(
-                id => id.Value.ToString(),
-                value => new ProjectId(Guid.Parse(value)))
-            .ValueGeneratedNever();
-
-        //ConversionExtensions.GuidIdToStringConverter<ProjectId>();
-        //ConversionExtensions.StringToGuidIdConverter<ProjectId>());
-
+        builder.Property(x => x.Id).HasEntityIdGuidConversion();
         builder.HasOne<ApplicationUser>().WithMany().HasForeignKey(x => x.UserId).OnDelete(DeleteBehavior.Restrict);
     }
-}
-
-public static class ConversionExtensions
-{
-    public static Expression<Func<TId, string>> GuidIdToStringConverter<TId>() =>
-        (TId id) => id.GetType().GetProperty("Value").GetValue(id) as string;
-
-    public static Expression<Func<string, TId>> StringToGuidIdConverter<TId>() =>
-        (string idValue) => (TId) Activator.CreateInstance(typeof(TId), Guid.Parse(idValue));
 }
---
+++ src/flashcards-blazor/Flashcards.WebApp/Shared/EntityFrameworkCore/PropertyBuilderExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace Flashcards.WebApp.Shared.EntityFrameworkCore;
+
+public static class PropertyBuilderExtensions
+{
+    public static ValueConverter<TId, string> GuidIdToStringConverter<TId>() =>
+        new(id => id.GetType().GetProperty("Value").GetValue(id).ToString(),
+            str => (TId)Activator.CreateInstance(typeof(TId), Guid.Parse(str)));
+
+    public static PropertyBuilder<T> HasEntityIdGuidConversion<T>(this PropertyBuilder<T> builder) =>
+        builder.HasConversion(GuidIdToStringConverter<T>());
+}
